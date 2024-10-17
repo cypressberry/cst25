e1091f897b2bb0bd4516a678bb679bcb46958298
cypressberry <carlysarahlynn@icloud.com>
2024-10-17 14:45:36 -0700

    new SHTUFF

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Carly Hunter
+// Date: 10/17/24
 
 // Constants
 
@@ -19,3 +19,24 @@ function main() {
 
 // let's get this party started
 main();
+// Guess_The_Number_Game
+
+// 1. Initialize the Game
+//    - Display Instructions
+//    - Generate the number (range 1-100)
+
+// 2. User Input
+//    - Prompt player to give a guess
+
+// 3. Validate Input
+//    - Check if guess is a valid number within the 1-100 range
+
+// 4. Give Feedback
+//    - If the guess is too high, tell the player it's too high
+//    - If the guess is too low, tell the player it's too low
+//    - If the guess is correct, congratulate the player
+
+// 5. Ask if the Player Would Like to Play Again
+//    - After a correct guess has been made, ask the player if they’d like to go again
+//    - If yes, reset the game and generate a new random number
+//    - If no, end the game
