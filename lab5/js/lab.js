a67e5d1ae0eb1b4c9f5efda4db9002dc07e27095
cypressberry <carlysarahlynn@icloud.com>
2024-10-21 18:45:54 -0700

    Javasquipt

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,28 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - This the JS for the lab5 assignment
+// Author: Ashan Devine
+// Date: October 19th 2024
 
-// Constants
+// Variable Definitions
+make = "Honda";
+model = "Accord";
+color = "Silver";
+year = 2017;
+age = (2024 - year);
+ownIt = true;
+
 
 // Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
 function main() {
   console.log("Main function started.");
-  // the code that makes everything happen
+  // Output
+  document.writeln("Make: " + make + "<br>");
+  document.writeln("Model: " + model + "<br>");
+  document.writeln("Color: " + color + "<br>");
+  document.writeln("Year: " + year + "<br>");
+  document.writeln("Age: " + age + "<br>");
+  document.writeln("Owned: " + ownIt + "<br>");
 }
+
 
 // let's get this party started
 main();
