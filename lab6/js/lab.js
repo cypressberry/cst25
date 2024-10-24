b52969955204ccb234b38d5b106c585c0cb4fa4e
cypressberry <carlysarahlynn@icloud.com>
2024-10-24 13:09:50 -0700

    alot.. ALOT

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,20 +1,28 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - This is the logic file for the lab6 assignment
+// Author: Carly Hunter
+// Date: 10/24/24
 
-// Constants
+// Declarations:
+
+let myTransport=["EBike", "Rides from my friends", "My Legs"];
+
+let myMainRide = {
+  make: "Radpower",
+  model:"Radrunner",
+  color: "Green",
+  year: "2020",
+  age: function() {
+      return 2024 - this.year;
+      }
+  };
 
 // Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
+  document.writeln("For getting around, I use: " + myTransport + "<br>");
+  document.writeln("My Main Ride: <pre>", 
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
 }
 
 // let's get this party started
