b8dc1cf1532926d8528f22080067a66ea872f687
cypressberry <carlysarahlynn@icloud.com>
2024-10-29 00:23:15 -0700

    WAA

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,66 +1,29 @@
 /*
-// index.js - This is the logic file for the lab6 assignment
+// index.js - This is the logic file for the lab7 assignment
  * Author: Carly Hunter <casahunt@ucsc.edu>
- * Date: 10/24/24
+ * Date: 10/28/24
  */
 
 
-function shuffleArray(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+// Constants
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
+// Functions
 
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-  }
-  return array;
-}
-
-function toTitleCase(str) {
-  return str.replace(
-      /\w\S*/g,
-      function(txt) {
-          return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-      }
-  );
-}
-
-function sortUserName(userName) {
-
-  return userName.toLowerCase().split("").sort().join("");
-}
-
-// sortUserName - a function that takes user input and sorts the letters
-// of their name
-function randomizeName(userName) {
-  // convert userName string to an array
-  var nameArray = userName.toLowerCase().split("");
-  console.log("nameArray =", nameArray);
-  // shuffle array with our shuffle function
-  var shuffledArray = shuffleArray(nameArray);
-  console.log("shuffledArray =", shuffledArray);
-  var shuffledString = shuffledArray.join("");
-  // shift to Title Case (like a name)
-  var newName = toTitleCase(shuffledString);
-  // return array to a string
-  return newName;
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function getSortedName(person) {
+  // some code here
+  let sortedName = person.split("").sort().join("");  //Take the name and split, sort, and join into a string
+  
+  // return results;
+  return sortedName;
 }
 
 function main() {
-  var userName = window.prompt("give me a word, any word.");
-  document.writeln("<style>:root {--textlen: " + userName.length + ";}</style>");
-  var sortedName = sortUserName(userName);
-  document.writeln("Here's your sorted word: " + sortedName + "</br></br>");
-  var randomName = randomizeName(userName);
-  document.writeln("would you look at that.<br>new word above.");
-  document.writeln("<div class='name'>" + randomName + "</div>")
+  console.log("Main function started.");
+  let person = prompt("Please enter your name"); //Launch a prompt for the user to enter their name
+  document.writeln("Your sorted name is: " + getSortedName(person)); //Print string + result of function with name passed as an arg
+  // the code that makes everything happen
 }
 
-// call the function
+// let's get this party started
 main();
