b56f128d2a558223d43cef82b2f6bcbb24691500
cypressberry <carlysarahlynn@icloud.com>
2024-10-29 18:33:22 -0700

    yipppeee

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,28 +1,28 @@
-// index.js - This is the logic file for the lab6 assignment
-// Author: Carly Hunter
-// Date: 10/24/24
+/*
+// index.js - This is the logic file for the lab7 assignment
+ * Author: Carly Hunter <casahunt@ucsc.edu>
+ * Date: 10/28/24
+ */
 
-// Declarations:
 
-let myTransport=["EBike", "Rides from my friends", "My Legs"];
-
-let myMainRide = {
-  make: "Radpower",
-  model:"Radrunner",
-  color: "Green",
-  year: "2020",
-  age: function() {
-      return 2024 - this.year;
-      }
-  };
+// Constants
 
 // Functions
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function getSortedName(person) {
+  // some code here
+  let sortedName = person.split("").sort().join("");  //Take the name and split, sort, and join into a string
+  
+  // return results;
+  return sortedName;
+}
+
 function main() {
   console.log("Main function started.");
+  let person = prompt("Please enter your name"); //Launch a prompt for the user to enter their name
+  document.writeln("Your sorted name is: " + getSortedName(person)); //Print string + result of function with name passed as an arg
   // the code that makes everything happen
-  document.writeln("For getting around, I use: " + myTransport + "<br>");
-  document.writeln("My Main Ride: <pre>", 
-    JSON.stringify(myMainRide, null, '\t'), "</pre>");
 }
 
 // let's get this party started
