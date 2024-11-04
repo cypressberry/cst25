810180929724d992fc5be6a738d2f3e19d9af700
cypressberry <carlysarahlynn@icloud.com>
2024-11-04 12:57:25 -0800

    lots of stuff

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,29 +1,39 @@
 /*
-// index.js - This is the logic file for the lab7 assignment
+// index.js - This is the logic file for the lab9 assignment
  * Author: Carly Hunter <casahunt@ucsc.edu>
- * Date: 10/28/24
+ * Date: 11/04/24
  */
+// Using jQuery to add buttons to each section
+const defaultAudio = document.getElementById('default-audio');
+const partyAudio = document.getElementById('party-audio');
 
 
-// Constants
+$("#party-toggle button").click(function() {
+    if ($("body").hasClass("party")) {
+        // Switch back to default mode
+        $("body").removeClass("party");
+        $("#party-toggle button").html("Party");
+        partyAudio.pause();
+        partyAudio.currentTime = 0; // Reset party audio
+        defaultAudio.play(); // Start default audio
+    } else {
+        // Switch to party mode
+        $("body").addClass("party");
+        $("#party-toggle button").html("Professional");
+        defaultAudio.pause();
+        defaultAudio.currentTime = 0; // Reset default audio
+        partyAudio.play(); // Start party audio
+    }
+});
 
-// Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function getSortedName(person) {
-  // some code here
-  let sortedName = person.split("").sort().join("");  //Take the name and split, sort, and join into a string
-  
-  // return results;
-  return sortedName;
-}
+$(".sectionbox").append("<button class='btn btn-secondary highlight-button'>Toggle Highlight</button>");
 
-function main() {
-  console.log("Main function started.");
-  let person = prompt("Please enter your name"); //Launch a prompt for the user to enter their name
-  document.writeln("Your sorted name is: " + getSortedName(person)); //Print string + result of function with name passed as an arg
-  // the code that makes everything happen
-}
 
-// let's get this party started
-main();
+$(document).on("click", ".highlight-button", function() {
+    $(this).parent().toggleClass("special");
+});
+
+$("#make-hide").click(function() {
+    $("#instructions").toggleClass("hidden");
+});
