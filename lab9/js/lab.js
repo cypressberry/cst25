39f1136b7c80a70e99a2da9db507f58335c4ec8f
cypressberry <carlysarahlynn@icloud.com>
2024-11-04 17:50:25 -0800

    lol

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -8,32 +8,34 @@ const defaultAudio = document.getElementById('default-audio');
 const partyAudio = document.getElementById('party-audio');
 
 
-$("#party-toggle button").click(function() {
+$("#party-toggle button").click(function() {  //Toggles the party mode
     if ($("body").hasClass("party")) {
         // Switch back to default mode
         $("body").removeClass("party");
-        $("#party-toggle button").html("Party");
-        partyAudio.pause();
+        $("#party-toggle button").html("Party");  //Updates button to provide party option
+        partyAudio.pause(); //Pauses party audio
         partyAudio.currentTime = 0; // Reset party audio
         defaultAudio.play(); // Start default audio
     } else {
         // Switch to party mode
-        $("body").addClass("party");
-        $("#party-toggle button").html("Professional");
-        defaultAudio.pause();
+        $("body").addClass("party");  //Adds class to body, party
+        $("#party-toggle button").html("Professional"); //Updates the button to provide professional option
+        defaultAudio.pause(); //Pauses party audio
         defaultAudio.currentTime = 0; // Reset default audio
         partyAudio.play(); // Start party audio
     }
 });
 
 
-$(".sectionbox").append("<button class='btn btn-secondary highlight-button'>Toggle Highlight</button>");
+$(".sectionbox").append("<button class='btn btn-secondary highlight-button'>Toggle Highlight</button>");  //Adds buttons to sections
 
 
-$(document).on("click", ".highlight-button", function() {
-    $(this).parent().toggleClass("special");
+$(document).on("click", ".highlight-button", function() { //Add toggle class functionality to button
+    $(this).parent().toggleClass("special");  // Toggles the "special" class on the parent section of the clicked button
 });
 
+
+// Click event handler for the "make-hide" button
 $("#make-hide").click(function() {
-    $("#instructions").toggleClass("hidden");
+    $("#instructions").toggleClass("hidden"); // Toggles the "hidden" class on the element with ID "instructions"
 });
